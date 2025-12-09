f1e0d7d194f688794f137767e1c2bac281c31feb
tik-tock-clock <overdrive6754@gmail.com>
2025-12-09 18:41:38 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-mkdir chat-program-client
-cd chat-program-client
-
-git clone https://github.com/tik-tock-clock/my-python-chat-program.git git-repo
-cd git-repo
 python3 -m venv .venv
 source .venv/bin/activate
 
@@ -12,6 +7,4 @@ pip install textual pyinstaller
 pyinstaller -F --add-data="style.tcss:./"
 deactivate
 
-cd ..
-mv ./git-repo/dist/client ./
-mv ./git-repo/style.tcss ./
+mv ./dist/client ./
