fca4f3b5c731f82de449d36a68672bedbfe895e0
tik-tock-clock <overdrive6754@gmail.com>
2025-12-09 18:54:47 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install textual pyinstaller
 
-pyinstaller -F --add-data="style.tcss:./"
+pyinstaller -F --add-data="style.tcss:./" client.py
 deactivate
 
-mv ./dist/client ./
+mv ./dist/client ./sudo apt install -f
